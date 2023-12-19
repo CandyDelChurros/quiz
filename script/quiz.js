a58f9e9845ff6b5b9f7d12c30d88046b2ee0084c
CandyDelChurros <cayo.veiga54@gmail.com>
2023-12-18 22:41:33 -0400

    Quiz final

---
+++ script/quiz.js
@@ -0,0 +1,66 @@
+function ready(){
+    document.getElementById('titulo').style.visibility = 'hidden'
+    document.getElementById('p1').style.visibility = 'visible'
+    document.getElementById('q1').style.visibility = 'visible'
+}
+
+function q1(){
+    document.getElementById('q1').style.visibility = 'hidden'
+    document.getElementById('p1').style.visibility = 'hidden'
+    document.getElementById('p2').style.visibility = 'visible'
+    document.getElementById('q2').style.visibility = 'visible'
+}
+function q2(){
+    document.getElementById('q2').style.visibility = 'hidden'
+    document.getElementById('p2').style.visibility = 'hidden'
+    document.getElementById('p3').style.visibility = 'visible'
+    document.getElementById('q3').style.visibility = 'visible'
+}
+function q3(){
+    document.getElementById('q3').style.visibility = 'hidden'
+    document.getElementById('p3').style.visibility = 'hidden'
+    document.getElementById('p4').style.visibility = 'visible'
+    document.getElementById('q4').style.visibility = 'visible'
+}
+function q4(){
+    document.getElementById('q4').style.visibility = 'hidden'
+    document.getElementById('p4').style.visibility = 'hidden'
+    document.getElementById('p5').style.visibility = 'visible'
+    document.getElementById('q5').style.visibility = 'visible'
+}
+function q5(){
+    document.getElementById('q5').style.visibility = 'hidden'
+    document.getElementById('p5').style.visibility = 'hidden'
+    document.getElementById('p6').style.visibility = 'visible'
+    document.getElementById('q6').style.visibility = 'visible'
+}
+function q6(){
+    document.getElementById('q6').style.visibility = 'hidden'
+    document.getElementById('p6').style.visibility = 'hidden'
+    document.getElementById('p7').style.visibility = 'visible'
+    document.getElementById('q7').style.visibility = 'visible'
+}
+function q7(){
+    document.getElementById('q7').style.visibility = 'hidden'
+    document.getElementById('p7').style.visibility = 'hidden'
+    document.getElementById('p8').style.visibility = 'visible'
+    document.getElementById('q8').style.visibility = 'visible'
+}
+function q8(){
+    document.getElementById('q8').style.visibility = 'hidden'
+    document.getElementById('p8').style.visibility = 'hidden'
+    document.getElementById('p9').style.visibility = 'visible'
+    document.getElementById('q9').style.visibility = 'visible'
+}
+function q9(){
+    document.getElementById('q9').style.visibility = 'hidden'
+    document.getElementById('p9').style.visibility = 'hidden'
+    document.getElementById('p10').style.visibility = 'visible'
+    document.getElementById('q10').style.visibility = 'visible'
+}
+function q10(){
+    document.getElementById('q9').style.visibility = 'hidden'
+    document.getElementById('p9').style.visibility = 'hidden'
+    document.getElementById('p10').style.visibility = 'visible'
+    document.getElementById('q10').style.visibility = 'visible'
+}
---
+++ script/script.js
@@ -0,0 +1,8 @@
+function intro(){
+    document.getElementById("botao").style.visibility = 'hidden'
+    document.getElementById("instrucoes").style.visibility = 'visible'
+}
+function voltar(){
+    document.getElementById("botao").style.visibility = 'visible'
+    document.getElementById("instrucoes").style.visibility = 'hidden'
+}
